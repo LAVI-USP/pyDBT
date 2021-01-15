f194b6650980d5f277725d51a188eab0b8a75fa2
rodrigovimieiro <rodrigo.vimieiro@gmail.com>
2021-01-15 17:19:44 -0300

    Virtual phantom added (Shepp Logan)

--- pydbt/parameterSettings.py
+++ pydbt/parameterSettings.py
@@ -8,35 +8,12 @@ Created on Mon Jan 13 15:19:22 2020
      
 class geometry_settings():
     
-    # def __init__(self):
-        
-        # Intialize some empty variables for later on
-        # self.nx = None
-        # self.ny = None    
-        # self.nz = None    
-        # self.nu = None   
-        # self.nv = None   
-        # self.dx = None 
-        # self.dy = None
-        # self.dz = None
-        # self.du = None   
-        # self.dv = None
-        # self.DSD = None                           
-        # self.DSO = None                          
-        # self.DDR = None                            
-        # self.DSR = None          
-        # self.DAG = None                
-        # self.nProj = None  
-        # self.tubeAngle = None
-        # self.detAngle = None
-        
-    
     def GE(self):
          
         # Breast voxels density 
         self.nx = 1058    # number of voxels (columns)
         self.ny = 1978    # number of voxels (rows)
-        self.nz = 107    # number of voxels (slices)
+        self.nz = 107     # number of voxels (slices)
         
         # Detector panel pixel density
         self.nu = 2394    # number of pixels (columns)
@@ -66,4 +43,40 @@ class geometry_settings():
         self.tubeAngle = 25     # Tube Angle
         
         self.detAngle = 0       # Detector Angle
- 
+            
+    def SheppLogan(self):
+         
+        # Breast voxels density 
+        self.nx = 250    # number of voxels (columns)
+        self.ny = 250    # number of voxels (rows)
+        self.nz = 200    # number of voxels (slices)
+        
+        # Detector panel pixel density
+        self.nu = 216    # number of pixels (columns)
+        self.nv = 650    # number of pixels (rows)
+        
+        # Single voxel real size (mm)
+        self.dx = 0.112 
+        self.dy = 0.112
+        self.dz = 1
+        
+        # Single detector real size (mm)
+        self.du = 0.14
+        self.dv = 0.14
+        
+        # X-ray source and detector distances
+        self.DSD = 700                           # Distance from source to detector (mm)
+        self.DSO = 597                           # Distance from source to the top of object (mm)
+        self.DDR = 0                             # Distance from detector to pivot (mm)
+        self.DSR = self.DSD - self.DDR           # Distance from source to pivot (mm)
+        self.DAG = 25                            # Distance of Air Gap (mm)         
+        
+        
+        # Number of Projections
+        self.nProj = 15  
+        
+        # Angle settings (Degrees)
+        self.tubeAngle = 15     # Tube Angle
+        
+        self.detAngle = 4.2     # Detector Angle
+        
