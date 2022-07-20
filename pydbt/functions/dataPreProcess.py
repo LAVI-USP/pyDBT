75db86ca4d25292f46c5f8d16a4c3111fe543713
rodrigovimieiro <rodrigo.vimieiro@gmail.com>
2022-07-20 09:19:08 -0300

    Alternative method to segment

--- pydbt/functions/dataPreProcess.py
+++ pydbt/functions/dataPreProcess.py
@@ -8,6 +8,7 @@ Created on Tue Oct 22 10:34:24 2019
 
 import numpy as np
 from scipy.ndimage.filters import uniform_filter1d
+from skimage.filters import threshold_otsu
 
 def dataPreProcess(proj, geo, flagtransfIntensity=True, flagCropProj=True):
     
@@ -38,7 +39,25 @@ def cropProj(proj):
     secondDv = np.gradient(uniform_filter1d(firstDv, size=100))
     
     # Takes its min second derivative
-    indX = np.argmin(secondDv) - Gap 
+    indX = np.argmin(secondDv)  
+    
+    # Alternative method to compare
+    # Otsu
+    thresh = threshold_otsu(proj[:,:,proj.shape[2]//2 - 1])
+    
+    # Get mask
+    mask = proj[:,:,proj.shape[2]//2 - 1] < thresh
+    
+    # Weight bounds
+    mask_w = np.sum(mask, 0) > 0
+    res = np.where(mask_w == True)
+    w_min, w_max = res[0][0], res[0][-1]
+    
+    # If the difference is big, take the min (most left)
+    if np.abs(indX - w_min) > 200:
+        indX = np.min([indX, w_min])
+    
+    indX -= Gap
         
     proj_crop = proj[:,indX::,:]
     		    
